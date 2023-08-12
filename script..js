c647cf236eab07956e8f20c734efaefcba77b404
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2023-08-12 18:00:16 +0500

    comments added

--- script..js
+++ script..js
@@ -1,10 +1,11 @@
+// this function display greetings 
 function greeting(str){
     console.log(str);
 }
 
 greeting('hi');
 
-
+// this function displays the name 
 function name (str) {
  console.log(str) 
  
