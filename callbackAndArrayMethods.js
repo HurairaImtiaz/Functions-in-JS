8f3f189bcdc575854c10c132f3847b34125c34d9
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2023-08-17 13:14:15 +0500

    finding Even numbers using ForEach

--- callbackAndArrayMethods.js
+++ callbackAndArrayMethods.js
@@ -3,3 +3,11 @@ numbers.forEach(function(element){
     console.log(element);
 
 })
+
+numbers.forEach(function(element){
+    if (element % 2 === 0)
+    {
+        console.log(element);
+    }
+
+})
